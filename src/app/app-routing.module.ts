5b3849f923976b345f36eadd8518d6abf4677d2c
Moheb619 <6moheb19@gmail.com>
2022-12-14 03:03:01 +0600

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }, { path: 'Cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {}
