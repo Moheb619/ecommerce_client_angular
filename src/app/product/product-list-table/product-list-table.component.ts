f5a8f5a4f06409915c872f2b6392370a817bc790
Saeed Hossain Moheb <6moheb19@gmail.com>
2022-12-14 16:28:36 +0600

    second commit

---
+++ src/app/product/product-list-table/product-list-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-table',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list-table.component.css']
+})
+export class ProductListTableComponent {
+
+}
